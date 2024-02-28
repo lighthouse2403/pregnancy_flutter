0d9e450c06d2bb83c2ad97984c61db3f1dbdbb57
Beacon <haidang2403@gmail.com>
2024-02-28 10:42:26 +0700

    no message

--- lib/music/music.dart
+++ lib/music/music.dart
@@ -1,82 +1,273 @@
+import 'package:audio_session/audio_session.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:pregnancy_flutter/_gen/assets.gen.dart';
-import 'package:pregnancy_flutter/common/base/base_app_bar.dart';
+import 'package:flutter/services.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 import 'package:pregnancy_flutter/common/base/base_statefull_widget.dart';
 import 'package:pregnancy_flutter/music/audio_handler.dart';
-import 'package:pregnancy_flutter/music/model/music_model.dart';
-import 'package:pregnancy_flutter/music/music_row.dart';
-import 'package:pregnancy_flutter/routes/routes.dart';
+import 'package:rxdart/rxdart.dart';
 
 class Music extends BaseStatefulWidget {
   const Music({super.key});
   @override
-  State<Music> createState() => _KnowledgeState();
+  State<Music> createState() => _MusicState();
 }
 
-class _KnowledgeState extends BaseStatefulState<Music> {
-  final ScrollController _controller = ScrollController();
-
-  List<MusicModel> musics = [
-    MusicModel(name: 'Bản nhạc 1', path: 'pregnancy_sound_1', status: false),
-    MusicModel(name: 'Bản nhạc 2', path: 'pregnancy_sound_2', status: false),
-    MusicModel(name: 'Bản nhạc 3', path: 'pregnancy_sound_3', status: false),
-    MusicModel(name: 'Bản nhạc 4', path: 'pregnancy_sound_4', status: false),
-    MusicModel(name: 'Bản nhạc 5', path: 'pregnancy_sound_5', status: false),
-  ];
+class _MusicState extends State<Music> {
+  static int _nextMediaId = 0;
+  late AudioPlayer _player;
+  final _playlist = ConcatenatingAudioSource(children: [
+    ClippingAudioSource(
+      start: const Duration(seconds: 60),
+      end: const Duration(seconds: 90),
+      child: AudioSource.uri(Uri.parse('asset:///assets/audios/pregnancy_sound_1.mp3')),
+      tag: MediaItem(
+        id: '${_nextMediaId++}',
+        album: "Science Friday",
+        title: "pregnancy_sound_1",
+        artUri: Uri.parse('asset:///assets/audios/pregnancy_sound_1.mp3'),
+      ),
+    ),
+    AudioSource.uri(Uri.parse("asset:///assets/audios/pregnancy_sound_2.mp3"),
+      tag: MediaItem(
+        id: '${_nextMediaId++}',
+        album: "Science Friday",
+        title: "pregnancy_sound_2",
+        artUri: Uri.parse("asset:///assets/audios/pregnancy_sound_2.mp3"),
+      ),
+    ),
+    AudioSource.uri(Uri.parse("asset:///assets/audios/pregnancy_sound_3.mp3"),
+      tag: MediaItem(
+        id: '${_nextMediaId++}',
+        album: "Science Friday",
+        title: "pregnancy_sound_3",
+        artUri: Uri.parse("asset:///assets/audios/pregnancy_sound_3.mp3"),
+      ),
+    ),
+    AudioSource.uri(Uri.parse("asset:///assets/audios/pregnancy_sound_4.mp3"),
+      tag: MediaItem(
+        id: '${_nextMediaId++}',
+        album: "Science Friday",
+        title: "pregnancy_sound_4",
+        artUri: Uri.parse("asset:///assets/audios/pregnancy_sound_4.mp3"),
+      ),
+    ),
+  ]);
+  int _addedCount = 0;
 
   @override
   void initState() {
     super.initState();
-
+    _player = AudioPlayer();
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.black,
+    ));
+    _init();
   }
 
-  void initAudio() async {
-
+  Future<void> _init() async {
+    final session = await AudioSession.instance;
+    await session.configure(const AudioSessionConfiguration.speech());
+    // Listen to errors during playback.
+    _player.playbackEventStream.listen((event) {},
+        onError: (Object e, StackTrace stackTrace) {
+          print('A stream error occurred: $e');
+        });
+    try {
+      await _player.setAudioSource(_playlist);
+    } catch (e, stackTrace) {
+      // Catch load errors: 404, invalid url ...
+      print("Error loading playlist: $e");
+      print(stackTrace);
+    }
   }
 
   @override
-  PreferredSizeWidget? buildAppBar() {
-    return BaseAppBar(
-        title: 'Âm nhạc',
-        leading: InkWell(
-          onTap: () => Routes.instance.pop(),
-          child: Align(
-            alignment: Alignment.center,
-            child: Assets.icons.arrowBack.svg(width: 24, height: 24),
-          ),
-        )
-    );
+  void dispose() {
+    _player.dispose();
+    super.dispose();
   }
 
+  Stream<PositionData> get _positionDataStream =>
+      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
+          _player.positionStream,
+          _player.bufferedPositionStream,
+          _player.durationStream,
+              (position, bufferedPosition, duration) => PositionData(
+              position, bufferedPosition, duration ?? Duration.zero));
+
   @override
-  Widget? buildBody() {
-    return Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/pregnancy_backgroound_3.jpg'),
-              fit: BoxFit.cover
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: StreamBuilder<SequenceState?>(
+                  stream: _player.sequenceStateStream,
+                  builder: (context, snapshot) {
+                    final state = snapshot.data;
+                    if (state?.sequence.isEmpty ?? true) {
+                      return const SizedBox();
+                    }
+                    final metadata = state!.currentSource!.tag as MediaItem;
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Center(
+                                child:
+                                Image.network(metadata.artUri.toString())),
+                          ),
+                        ),
+                        Text(metadata.album!,
+                            style: Theme.of(context).textTheme.titleLarge),
+                        Text(metadata.title),
+                      ],
+                    );
+                  },
+                ),
+              ),
+              StreamBuilder<PositionData>(
+                stream: _positionDataStream,
+                builder: (context, snapshot) {
+                  final positionData = snapshot.data;
+                  return SeekBar(
+                    duration: positionData?.duration ?? Duration.zero,
+                    position: positionData?.position ?? Duration.zero,
+                    bufferedPosition:
+                    positionData?.bufferedPosition ?? Duration.zero,
+                    onChangeEnd: (newPosition) {
+                      _player.seek(newPosition);
+                    },
+                  );
+                },
+              ),
+              const SizedBox(height: 8.0),
+              Row(
+                children: [
+                  StreamBuilder<LoopMode>(
+                    stream: _player.loopModeStream,
+                    builder: (context, snapshot) {
+                      final loopMode = snapshot.data ?? LoopMode.off;
+                      const icons = [
+                        Icon(Icons.repeat, color: Colors.grey),
+                        Icon(Icons.repeat, color: Colors.orange),
+                        Icon(Icons.repeat_one, color: Colors.orange),
+                      ];
+                      const cycleModes = [
+                        LoopMode.off,
+                        LoopMode.all,
+                        LoopMode.one,
+                      ];
+                      final index = cycleModes.indexOf(loopMode);
+                      return IconButton(
+                        icon: icons[index],
+                        onPressed: () {
+                          _player.setLoopMode(cycleModes[
+                          (cycleModes.indexOf(loopMode) + 1) %
+                              cycleModes.length]);
+                        },
+                      );
+                    },
+                  ),
+                  Expanded(
+                    child: Text(
+                      "Playlist",
+                      style: Theme.of(context).textTheme.titleLarge,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  StreamBuilder<bool>(
+                    stream: _player.shuffleModeEnabledStream,
+                    builder: (context, snapshot) {
+                      final shuffleModeEnabled = snapshot.data ?? false;
+                      return IconButton(
+                        icon: shuffleModeEnabled
+                            ? const Icon(Icons.shuffle, color: Colors.orange)
+                            : const Icon(Icons.shuffle, color: Colors.grey),
+                        onPressed: () async {
+                          final enable = !shuffleModeEnabled;
+                          if (enable) {
+                            await _player.shuffle();
+                          }
+                          await _player.setShuffleModeEnabled(enable);
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 240.0,
+                child: StreamBuilder<SequenceState?>(
+                  stream: _player.sequenceStateStream,
+                  builder: (context, snapshot) {
+                    final state = snapshot.data;
+                    final sequence = state?.sequence ?? [];
+                    return ReorderableListView(
+                      onReorder: (int oldIndex, int newIndex) {
+                        if (oldIndex < newIndex) newIndex--;
+                        _playlist.move(oldIndex, newIndex);
+                      },
+                      children: [
+                        for (var i = 0; i < sequence.length; i++)
+                          Dismissible(
+                            key: ValueKey(sequence[i]),
+                            background: Container(
+                              color: Colors.redAccent,
+                              alignment: Alignment.centerRight,
+                              child: const Padding(
+                                padding: EdgeInsets.only(right: 8.0),
+                                child: Icon(Icons.delete, color: Colors.white),
+                              ),
+                            ),
+                            onDismissed: (dismissDirection) {
+                              _playlist.removeAt(i);
+                            },
+                            child: Material(
+                              color: i == state!.currentIndex
+                                  ? Colors.grey.shade300
+                                  : null,
+                              child: ListTile(
+                                title: Text(sequence[i].tag.title as String),
+                                onTap: () {
+                                  _player.seek(Duration.zero, index: i);
+                                },
+                              ),
+                            ),
+                          ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
-        child: CustomScrollView(
-          controller: _controller,
-          physics: const BouncingScrollPhysics(
-            parent: AlwaysScrollableScrollPhysics(),
-          ),
-          slivers: [
-            SliverList(
-              delegate: SliverChildBuilderDelegate((context, index) {
-                return InkWell(
-                  onTap: () {
-                  },
-                  child: MusicRow(music: musics[index]),
-                );
-              },
-                childCount: musics.length,
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            _playlist.add(AudioSource.uri(
+              Uri.parse("asset:///audio/nature.mp3"),
+              tag: MediaItem(
+                id: '${_nextMediaId++}',
+                album: "Public Domain",
+                title: "Nature Sounds ${++_addedCount}",
+                artUri: Uri.parse(
+                    "https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg"),
               ),
-            ),
-          ],
-        )
+            ));
+          },
+        ),
+      ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,22 +5,28 @@
 import FlutterMacOS
 import Foundation
 
-import audioplayers_darwin
+import audio_service
+import audio_session
 import cloud_firestore
 import firebase_analytics
 import firebase_core
 import firebase_crashlytics
+import just_audio
 import package_info_plus
 import path_provider_foundation
+import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
